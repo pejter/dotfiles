2128732b8baf12e72c965f91c03581f4f765e41e
Piotr Łuczak <piotrluczak1995@gmail.com>
2022-05-19 17:50:41 +0200

    fix dbus apps unable to find DISPLAY

--- sway/start.sh
+++ sway/start.sh
@@ -2,4 +2,6 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_DBUS_REMOTE=1
 
+dbus-update-activation-environment --systemd DISPLAY
+
 exec systemd-cat --identifier=sway sway
