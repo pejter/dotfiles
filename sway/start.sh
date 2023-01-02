05118df27206263654832ffafab85b4640dd3db5
Piotr Łuczak <piotrluczak1995@gmail.com>
2023-01-02 11:29:41 +0100

    Fix env dbus/systemd propagation

--- sway/start.sh
+++ sway/start.sh
@@ -2,6 +2,4 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_DBUS_REMOTE=1
 
-dbus-update-activation-environment --systemd DISPLAY
-
 exec systemd-cat --identifier=sway sway
