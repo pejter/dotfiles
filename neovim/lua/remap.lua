8811aec0906eb15f2fd1c8c532564112e51efe54
Piotr Łuczak <piotrluczak1995@gmail.com>
2024-02-17 05:05:47 +0100

    Add new keymaps and change to vim.keymap.set

--- neovim/lua/remap.lua
+++ neovim/lua/remap.lua
@@ -3,17 +3,22 @@ local wk = require("which-key")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-wk.register({
-	["<C-z>"] = { vim.cmd.UndotreeToggle, "Undo" },
-	["<C-f>"] = { "<cmd>Telescope live_grep<cr>", "Find" },
-	["<A-k>"] = { ":move -2<cr>", "Move up", mode = { "n", "i" } },
-	["<A-j>"] = { ":move +1<cr>", "Move down", mode = { "n", "i" } },
-	["<A-k>"] = { ":move '<-2<cr>gv", "Move up", mode = "v" },
-	["<A-j>"] = { ":move '>+1<cr>gv", "Move down", mode = "v" },
-})
+vim.keymap.set("n", "<C-z>", vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<C-f>", "<cmd>Telescope live_grep<cr>")
+
+vim.keymap.set("n", "J", "mzJ`z`")
+vim.keymap.set("n", "<A-k>", ":move .-2<cr>")
+vim.keymap.set("n", "<A-j>", ":move .+1<cr>")
+vim.keymap.set("i", "<A-k>", "<Esc>:move .-2<cr>gi")
+vim.keymap.set("i", "<A-j>", "<Esc>:move .+1<cr>gi")
+vim.keymap.set("v", "<A-k>", ":move '<-2<cr>gv")
+vim.keymap.set("v", "<A-j>", ":move '>+1<cr>gv")
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
 
 wk.register({
-	r = { "ry:%s/<C-r>r//gc<left><left><left>", "Replace", mode = "v" },
 	p = {
 		name = "Project",
 		l = { "<cmd>Telescope file_browser<cr>", "List" },
@@ -26,4 +31,10 @@ wk.register({
 		s = { vim.cmd.Git, "Status" },
 		o = { "<cmd>Telescope git_files<cr>", "Open File" },
 	},
+	x = { "<Cmd>!chmod +x %<cr>", "Set executable flag on current file" }
 }, { prefix = "<leader>" })
+
+wk.register({
+	p = { "\"_dP", "Paste (without replacing current register)" }
+}, { prefix = "<leader>", mode = "x" })
+
