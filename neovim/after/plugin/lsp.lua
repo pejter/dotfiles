d88a7870706e669d4ef0e07b41080da390bbf8f4
Piotr Łuczak <piotrluczak1995@gmail.com>
2024-05-27 11:33:39 +0200

    Remove lsp-zero

--- neovim/after/plugin/lsp.lua
+++ neovim/after/plugin/lsp.lua
@@ -1,14 +1,40 @@
-local lsp = require("lsp-zero")
+-- note: diagnostics are not exclusive to lsp servers
+-- so these can be global keybindings
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
-lsp.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp.default_keymaps({buffer = bufnr})
-	lsp.buffer_autoformat()
-end)
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'LSP actions',
+	callback = function(event)
+		local opts = {buffer = event.buf}
 
-require("mason").setup({})
-require("mason-lspconfig").setup({
+		-- these will be buffer-local keybindings
+		-- because they only work if you have an active language server
+
+		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+		vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+		vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+		vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+		vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+		vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+		vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+		vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+		vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+	end
+})
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local default_setup = function(server)
+	require('lspconfig')[server].setup({
+		capabilities = lsp_capabilities,
+	})
+end
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
 	ensure_installed = {
 		"tsserver",
 		"pylsp",
@@ -18,13 +44,31 @@ require("mason-lspconfig").setup({
 		"docker_compose_language_service",
 	},
 	handlers = {
-		lsp.default_setup,
+		default_setup,
+		lua_ls = function ()
+			require('lspconfig').lua_ls.setup({
+				capabilities = lsp_capabilities,
+				settings = {
+					Lua = {
+						runtime = {
+							version = 'LuaJIT'
+						},
+						diagnostics = {
+							globals = {'vim'},
+						},
+						workspace = {
+							library = {
+								vim.env.VIMRUNTIME,
+							}
+						}
+					}
+				}
+			})
+		end
 	},
 })
 
 local cmp = require('cmp')
-local cmp_format = require('lsp-zero').cmp_format()
-local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
 	preselect = 'item',
@@ -32,10 +76,12 @@ cmp.setup({
 		completeopt = 'menu,menuone,noinsert'
 	},
 	formatting = cmp_format,
-	sources = {
-		{name = 'nvim_lsp'},
-		{name = 'nvim_lua'},
-	},
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'luasnip' },
+	}, {
+		{ name = 'buffer' },
+	}),
 	mapping = cmp.mapping.preset.insert({
 		-- start & confirm completion
 		['<C-Space>'] = cmp.mapping.complete(),
@@ -43,11 +89,52 @@ cmp.setup({
 
 		-- scroll up and down the documentation window
 		['<C-u>'] = cmp.mapping.scroll_docs(-4),
-		['<C-d>'] = cmp.mapping.scroll_docs(4),   
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
 
 		-- Tab completion
-		['<Tab>'] = cmp_action.tab_complete(),
-		['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+		['<Tab>'] = {
+			c = function()
+				local cmp = require('cmp')
+				if cmp.visible() then
+					cmp.select_next_item()
+				else
+					cmp.complete()
+				end
+			end,
+		},
+		['<S-Tab>'] = {
+			c = function()
+				local cmp = require('cmp')
+				if cmp.visible() then
+					cmp.select_prev_item()
+				else
+					cmp.complete()
+				end
+			end,
+		},
+	}),
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+})
 
-	})
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{ name = 'cmdline' }
+	}),
+	matching = { disallow_symbol_nonprefix_matching = false }
 })
--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -18,11 +18,13 @@ local plugins = {
 	{ "williamboman/mason.nvim" },
 	{ "williamboman/mason-lspconfig.nvim" },
 
-	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
-	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
 	{ "hrsh7th/cmp-nvim-lsp" },
 	{ "hrsh7th/cmp-nvim-lua" },
-	{ "hrsh7th/nvim-cmp" },
+	{ "neovim/nvim-lspconfig" },
 	{ "L3MON4D3/LuaSnip" },
 }
 
