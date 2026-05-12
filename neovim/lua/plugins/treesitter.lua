040071864ee2558c33fada44984599439ba0c27b
Piotr Łuczak <pluczak@bolt.com>
2026-05-12 02:19:13 +0200

    [NeoVIM] Treesitter config

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -36,26 +36,32 @@ return {
 	},
 	config = function()
 		-- replicate `ensure_installed`, runs asynchronously, skips existing languages
-		require("nvim-treesitter").install(languages)
+		local ts = require("nvim-treesitter")
+		ts.install(languages)
 
 		vim.api.nvim_create_autocmd("FileType", {
 			group = vim.api.nvim_create_augroup("treesitter.setup", {}),
-			callback = function(args)
-				local buf = args.buf
-				local filetype = args.match
+			desc = "Auto-install and configure treesitter",
+			callback = function(event)
+				local buf = event.buf
+				local filetype = event.match
 
 				-- check if a parser exists for the current language
-				local language = vim.treesitter.language.get_lang(filetype) or filetype
-				if not vim.treesitter.language.add(language) then
+				local lang = vim.treesitter.language.get_lang(filetype) or filetype
+				if not vim.list_contains(ts.get_available(), lang) then
 					return
 				end
+
+				if not vim.list_contains(ts.get_installed(), lang) and not vim.list_contains(languages, lang) then
+					ts.install(lang):wait()
+				end
+
+				-- replicate `highlight = { enable = true }`
+				vim.treesitter.start(buf, lang)
 
 				-- replicate `fold = { enable = true }`
 				vim.wo.foldmethod = "expr"
 				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-
-				-- replicate `highlight = { enable = true }`
-				vim.treesitter.start(buf, language)
 
 				-- replicate `indent = { enable = true }`
 				vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
